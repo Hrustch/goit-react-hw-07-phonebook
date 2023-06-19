715dd9ba33ec80204a70a4207dde26863159ef6f
Hrustch <nik.stalker47@gmail.com>
2023-06-19 16:22:48 +0300

    warn fix

--- src/components/PhonebookContacts.jsx
+++ src/components/PhonebookContacts.jsx
@@ -1,6 +1,6 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { deleteContact, fetchContacts } from 'redux/contactsOperations';
+import { deleteContact } from 'redux/contactsOperations';
 
 
 export const PhonebookContacts = () => {
